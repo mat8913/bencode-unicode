28f8677541932e1067b0d5f3349d4948ef26e153
Matthew Harm Bekkema <mbekkema97@gmail.com>
2016-10-23 18:27:56 +1100

    Add Functor instance for BEncode

--- Data/BEncode/Type.hs
+++ Data/BEncode/Type.hs
@@ -7,3 +7,9 @@ data BEncode a = BInt Integer
                | BList [BEncode a]
                | BDict [(a , BEncode a)]
     deriving (Eq, Ord, Read, Show)
+
+instance Functor BEncode where
+  fmap _ (BInt x) = BInt x
+  fmap f (BString x) = BString (f x)
+  fmap f (BList xs) = BList $ map (fmap f) xs
+  fmap f (BDict xs) = BDict $ map (\(k, v) -> (f k, fmap f v)) xs
